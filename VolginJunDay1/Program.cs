225e00f7b04365414e0bf966635d05608b45f0ce
bmvolf <bmvolgin@gmail.com>
2023-08-11 18:00:35 +0300

    added hello

--- VolginJunDay1/Program.cs
+++ VolginJunDay1/Program.cs
@@ -11,7 +11,10 @@ namespace VolginJunDay1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
         }
     }
 }
